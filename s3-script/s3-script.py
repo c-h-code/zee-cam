c3a1ef665b84daafee03b7203ee10f9f240a09b1
c-h-code <connorhillis@protonmail.com>
2026-03-01 17:25:00 -0500

    reworked s3-script to work with a dev account

--- s3-script/s3-script.py
+++ s3-script/s3-script.py
@@ -1,36 +1,30 @@
 #!/home/ronnoc/workspace/zee-cam/s3-script/venv/bin/python3.13
-import time
+
 
 import boto3
 from botocore.exceptions import ClientError
 import os
 import sys
 import glob
-from dotenv import load_dotenv
-load_dotenv()
 
-AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION = os.getenv("AWS_REGION")
-BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+print("running")
+profile = os.getenv("AWS_PROFILE")
+print(profile)
+if profile == "dev":
+    BUCKET_NAME = "zee-vid-repo-dev"
 
 
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=AWS_ACCESS_KEY,
-    aws_secret_access_key=AWS_SECRET_KEY,
-    region_name=AWS_REGION             
-)
-
+session = boto3.Session()
+s3 = session.client("s3") 
 control_flag = s3.get_object(Bucket=BUCKET_NAME, Key ="control/run_flag.txt")['Body'].read().decode('utf-8')
 file = sys.argv[1]
 
-if control_flag:
+if control_flag == "True":
     try:
         s3.upload_file(
             file,
             BUCKET_NAME,
-            os.path.basename(file),  
+            f"videos/{os.path.basename(file)}",  
             ExtraArgs={"Tagging": "autodelete=true"})
     
         os.remove(file)
